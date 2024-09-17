b042be8d60235fad5ec938ac40b7e113cbecf62a
KarolinaGajos <142922712+KarolinaGajos@users.noreply.github.com>
2024-09-17 12:24:03 +0200

    fiksa setter og printuser - test

--- bruker.java
+++ bruker.java
@@ -22,4 +22,20 @@ private abstract user{
     public String getRoleId() {
         return roleId;
     }
+    public void setUserId(int i) {
+        userId = i;
+    }
+    public void setUserName(String n) {
+        userName = n;
+    }
+    public void setRoleId(int r) {
+        roleId = r;
+    }
+
+    public void printUser(){
+        System.out.println("User ID: " + userId);
+        System.out.println("User Name: " + userName);
+        System.out.println("Role ID: " + roleId);
+    }
+    
 }
