3d615a09889c0bf3c7e28fe214efd928c4a4865f
Valtyr-Angel <143401012+Valtyr-Angel@users.noreply.github.com>
2024-10-13 19:01:41 +0200

    Revert "oppdatering av rbac"
    This reverts commit 3b57190eb5e35bced198dac79c0c0add11bd762d.

--- careconnect/src/main/java/com/careconnect/careconnect/CareconnectApplication.java
+++ careconnect/src/main/java/com/careconnect/careconnect/CareconnectApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CareconnectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CareconnectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CareconnectApplication.class, args);
+		
+	}
+
 }
