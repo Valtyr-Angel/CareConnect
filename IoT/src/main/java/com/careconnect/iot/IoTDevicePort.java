9f8503d2b7804811db296c20b2ec7ca0cced51a4
KarolinaGajos <142922712+KarolinaGajos@users.noreply.github.com>
2024-11-07 17:25:09 +0100

    IoT service
    opprettet ferdig service IoT for å kommunisere med port og adapter for å utføre operasjoner + noe forklarende kommenterer i koden.

--- IoT/src/main/java/com/careconnect/iot/IoTDevicePort.java
+++ IoT/src/main/java/com/careconnect/iot/IoTDevicePort.java
@@ -2,6 +2,21 @@ package com.careconnect.iot;
 
 import java.util.List;
 
+/**
+ * Definerer operasjoner for håndtering av IoT-enheter i CareConnect-systemet.
+ * 
+ * IoTDevicePort-grensesnittet beskriver de nødvendige metodene for å hente, opprette
+ * og oppdatere IoT-enheter. Dette grensesnittet implementeres av adaptere eller
+ * andre komponenter som håndterer datarelaterte operasjoner for IoT-enhetene.
+ * 
+ * Eksempel på bruk:
+ * <pre>
+ * IoTDevicePort devicePort = new IoTDeviceAdapter();  // Kan implementeres av en adapter
+ * IoTDevice device = devicePort.getDeviceById("001");
+ * devicePort.createDevice(new IoTDevice("002", "Hjerteovervåker", "ICU", "På"));
+ * </pre>
+ */
+
 public interface IoTDevicePort {
 
     IoTDevice getDeviceById(String id);  // Hent en enhet etter ID
---
+++ IoT/src/main/java/com/careconnect/iot/IoTDeviceService.java
@@ -0,0 +1,45 @@
+package com.careconnect.iot;
+
+
+import java.util.List;
+
+/**
+ * Tjenesteklasse for håndtering av operasjoner relatert til IoT-enheter.
+ * 
+ * IoTDeviceService fungerer som et mellomlag mellom IoTDevicePort (som er ansvarlig for
+ * datalagring og operasjoner på IoT-enhetene) og resten av applikasjonen. Denne klassen
+ * gir enkle metoder for å hente, opprette og oppdatere IoT-enheter ved å bruke
+ * IoTDevicePort. Den kan utvides for å legge til ekstra logikk i fremtidige krav.
+ * 
+ * Eksempel på bruk:
+ * <pre>
+ * IoTDeviceService deviceService = new IoTDeviceService(new IoTDeviceAdapter());
+ * IoTDevice device = deviceService.getDeviceById("001");
+ * deviceService.createDevice(new IoTDevice("003", "Temperaturmåler", "Kjøkken", "På"));
+ * </pre>
+ */
+
+public class IoTDeviceService {
+
+    private final IoTDevicePort ioTDevicePort;
+
+    public IoTDeviceService(IoTDevicePort ioTDevicePort) {
+        this.ioTDevicePort = ioTDevicePort;
+    }
+
+    public IoTDevice getDeviceById(String id) {
+        return ioTDevicePort.getDeviceById(id);  // Bruker IoTDevice som DTO
+    }
+
+    public List<IoTDevice> getAllDevices() {
+        return ioTDevicePort.getAllDevices();  // Bruker IoTDevice som DTO
+    }
+
+    public void createDevice(IoTDevice device) {
+        ioTDevicePort.createDevice(device);  // Bruker IoTDevice som DTO
+    }
+
+    public void updateDeviceStatus(String id, String newStatus) {
+        ioTDevicePort.updateDeviceStatus(id, newStatus);
+    }
+}
