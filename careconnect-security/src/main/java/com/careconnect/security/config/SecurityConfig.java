920f2af8f4f3255dc2782dea534b0a2336a74b13
Valtyr-Angel <143401012+Valtyr-Angel@users.noreply.github.com>
2024-11-12 12:04:01 +0100

    wip auth work

--- careconnect-security/src/main/java/com/careconnect/security/config/SecurityConfig.java
+++ careconnect-security/src/main/java/com/careconnect/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,52 +24,61 @@ import org.springframework.security.config.annotation.authentication.configurati
 @EnableWebSecurity
 public class SecurityConfig {
 
-    
-
-    @SuppressWarnings("deprecation")
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests(authorizeRequests ->
-                authorizeRequests
-                        //.requestMatchers( "/resources/**").permitAll()
-                        .requestMatchers("/login", "/resources/**", "/static/**", "/css/**", "/js/**").permitAll()
-                    .requestMatchers("/admin/**").hasRole("ADMIN")
-                    .requestMatchers("/user/**").hasRole("USER")
-                    .anyRequest().authenticated()
-            )
-            .formLogin(formLogin ->
-                formLogin
-                    .loginPage("/login")
-                    .successHandler(customAuthenticationSuccessHandler())
-                    .permitAll()
-                    
-            )
-            .logout(logout ->
-                logout.permitAll()
-            );
-        return http.build();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
+
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user")
-                .password(passwordEncoder.encode("user"))
-                .roles("USER")
-                .build());
-        manager.createUser(User.withUsername("admin")
-                .password(passwordEncoder.encode("admin"))
+    public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}
+
+
+    
+        @SuppressWarnings("deprecation")
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                .csrf().disable()
+                .authorizeRequests(authorizeRequests ->
+                    authorizeRequests
+                            //.requestMatchers( "/resources/**").permitAll()
+                            .requestMatchers("/login", "/resources/**", "/static/**", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                    formLogin
+                        .loginPage("/login")
+                        .successHandler(customAuthenticationSuccessHandler())
+                        .permitAll()
+                        
+                )
+                .logout(logout ->
+                    logout.permitAll()
+                );
+            return http.build();
+        }
+    
+        @Bean
+        public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+            manager.createUser(User.withUsername("user")
+                    .password(passwordEncoder.encode("user"))
+                    .roles("USER")
+                    .build());
+            manager.createUser(User.withUsername("admin")
+                    .password(passwordEncoder.encode("admin"))
                 .roles("ADMIN")
                 .build());
         return manager;
     }
 
-    @Bean
-        public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-}
+
 
     @Bean
     public CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler() {
