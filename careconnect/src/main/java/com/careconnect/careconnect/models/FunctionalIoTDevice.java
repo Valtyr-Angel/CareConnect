557e9a0d59e7472d77af79bb079d08a2f0fecc02
KarolinaGajos <142922712+KarolinaGajos@users.noreply.github.com>
2024-11-13 13:50:49 +0100

    fjernet .txt filer

--- careconnect/src/main/java/com/careconnect/careconnect/models/FunctionalIoTDevice.java
+++ careconnect/src/main/java/com/careconnect/careconnect/models/FunctionalIoTDevice.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
+
 @Component
 public class FunctionalIoTDevice extends IoTDevice {
     private boolean isOn;
