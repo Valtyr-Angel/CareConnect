f6479cec71d827bf2142f8561ffc78e01c924783
uffiulf <sofnbeat@gmail.com>
2024-11-14 03:03:45 +0100

    Api works in VScode -Updated SecurityConfig
    **Authentication method needed for GUI
    **

--- careconnect/src/main/java/com/careconnect/careconnect/controller/JsonFileController.java
+++ careconnect/src/main/java/com/careconnect/careconnect/controller/JsonFileController.java
@@ -1,7 +1,8 @@
-package com.careconnect.careconnect.GUI;
+package com.careconnect.careconnect.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.FileCopyUtils;
 import java.io.IOException;
