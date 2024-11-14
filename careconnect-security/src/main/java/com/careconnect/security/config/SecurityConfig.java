e58f97e452146ef922624973f116d77bab74591f
Olav Liljeberg <sofnbeat@gmail.com>
2024-11-14 16:50:50 +0100

    updated security- API should bypass authentication **(yes it should)

--- careconnect-security/src/main/java/com/careconnect/security/config/SecurityConfig.java
+++ careconnect-security/src/main/java/com/careconnect/security/config/SecurityConfig.java
@@ -15,78 +15,66 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import com.careconnect.security.handler.CustomAuthenticationSuccessHandler;
 
-
-
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-}
-
-
-    
-        @SuppressWarnings({ "deprecation", "removal" })
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                .csrf().disable()
-     .authorizeRequests(authorizeRequests ->
-       authorizeRequests
-           //.requestMatchers("/login", "/error", "/resources/**", "/static/**", "/css/**", "/js/**", 
-                   //"/application/static/**","/application/resources/**",
-                   //"/application/css/**","/application/templates/**").permitAll()
-                   
-                   .requestMatchers(  "/application/static/**","/application/resources/**",
-                   "/application/css/**","/application/templates/**").permitAll()
-           .requestMatchers("/admin/**").hasRole("ADMIN")
-           .requestMatchers("/user/**").hasRole("USER")
-           .anyRequest().authenticated()
-   )
-                .formLogin(formLogin ->
-                    formLogin
-                        .loginPage("/login")
-                        .successHandler(customAuthenticationSuccessHandler())
-                        .permitAll()
-                        
-                )
-                .logout(logout ->
-                    logout.permitAll()
-                );
-            return http.build();
-        }
-    
-        @Bean
-        public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-
-            manager.createUser(User.withUsername("user")
-                    .password(passwordEncoder.encode("user"))
-                    .roles("USER")
-                    .build());
-            manager.createUser(User.withUsername("admin")
-                    .password(passwordEncoder.encode("admin"))
-                .roles("ADMIN")
-                .build());
-        return manager;
+        return new BCryptPasswordEncoder();
     }
 
+    @SuppressWarnings({ "deprecation", "removal" })
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(
+                                        "/application/static/**", "/application/resources/**",
+                                        "/application/css/**", "/application/templates/**",
+                                        "/api/jsonfile" // Add your public endpoint here
+                                ).permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/user/**").hasRole("USER")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .successHandler(customAuthenticationSuccessHandler())
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout.permitAll()
+                );
+        return http.build();
+    }
 
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER")
+                .build());
+
+        manager.createUser(User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build());
+
+        return manager;
+    }
 
     @Bean
     public CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler() {
         return new CustomAuthenticationSuccessHandler();
     }
-
-    
-    
 }
--- careconnect/src/main/java/com/careconnect/careconnect/controller/JsonFileController.java
+++ careconnect/src/main/java/com/careconnect/careconnect/controller/JsonFileController.java
@@ -1,4 +1,4 @@
-package com.careconnect.careconnect.GUI;
+package com.careconnect.careconnect.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
