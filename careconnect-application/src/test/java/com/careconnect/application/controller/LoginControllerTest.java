e1e95c0066b034348084764314b9640412021e23
Valtyr-Angel <143401012+Valtyr-Angel@users.noreply.github.com>
2024-11-21 11:03:29 +0100

    POM ok

--- careconnect-application/src/test/java/com/careconnect/application/controller/LoginControllerTest.java
+++ careconnect-application/src/test/java/com/careconnect/application/controller/LoginControllerTest.java
@@ -3,13 +3,16 @@ package com.careconnect.application.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(LoginController.class) // Test LoginController specifically
+import com.careconnect.security.config.SecurityConfig;
+
+@WebMvcTest(LoginController.class)
+@ContextConfiguration(classes = {LoginController.class, SecurityConfig.class}) 
 public class LoginControllerTest {
 
     @Autowired
@@ -23,5 +26,3 @@ public class LoginControllerTest {
                .andExpect(view().name("login")); // Expect the "login" view
     }
 }
-
-
